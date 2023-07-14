b67066e4c12479947457b84b3b01ced4896352a0
khayru <43963328+khayru@users.noreply.github.com>
2023-07-14 03:58:05 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,56 @@
+console.log("Group Project");
+
+// create list cars
+
+// create car object hold property to accse the property of object use car[]
+
+car = {
+   
+    ModelYear: 2012,
+    Make:"CHEVROLET",
+    Model:"Volt",
+    VehiclePrimaryUse:"Passenger",
+    ElectricRange :35,
+    NewUsed:"Used",
+    SalePrice:0,
+    City: "BELLEVUE",
+    Stat:"WA",
+    PostalCode: 98007
+    }
+
+cars1.busg= ({
+
+    ModelYear: 2018,
+    Make: "TESLA",
+    Model:"Model 3",
+    VehiclePrimaryUs:"Passenger",
+    Electric_Range:215,
+    Vehicle: "New",
+    SalePrice:55000,
+    SaleDate:"May 23 2018",
+    TransactionType:"Original Title",
+    City: "SEATTLE", 
+    State:"WA", 
+    PostalCode:98105
+     })
+     
+cars.bush({
+        
+    Model Year:2018,
+    Make:"TESLA",
+    Model:"Model 3",
+    ElectricRange:215,
+    NeworUsedVehicle:"Used",
+    SalePrice: 0,
+    TransactionType:"Registration Renewal",
+    DOLTransactionDate:"April 18 2019",
+    TransactionYear:2019,
+    City:"SEATTLE",
+    State: "WA",
+    PostalCode:98105
+
+})
+// get how many cars
+car.length;
+///loob and print ou all the prices of cars
+for(let i = 0; i <)
